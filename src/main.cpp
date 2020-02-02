a0435139f6b7b782f7aec40eedd35be42b8c13f8
Lorenzo D'Agostino <lorenzodagostino@gmail.com>
2020-02-02 03:52:19 +0100

    Added DHT with upgraded protocol

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "DHT.h"
 
 #define SERIESRESISTOR 10000
 #define NOMINAL_RESISTANCE 10000
@@ -7,38 +8,31 @@
 #define TEMP_READINGS 10
 #define LIGHT_READINGS 5
 
-uint16_t light;
-uint8_t bt;
-uint16_t steinhart_uint16;
-uint8_t steinhart_high, steinhart_low;
-float ADCvalue, Resistance, steinhart, temperature;
+uint16_t temperature_uint16, light;
+uint8_t temperature_high, temperature_low, light_high, light_low, bt, humidity_uint8;
+float ADCvalue, Resistance, temperature, humidity, tmp_temperature, tmp_humidity;
+
+DHT dht;
 
 void setup() {
   pinMode(PA0, INPUT_ANALOG); // Light ADC
-  pinMode(PA4, INPUT_ANALOG); // Temperature ADC
   Serial3.begin(9600); // BT UART
   Serial1.begin(9600); // Communication UART
+
+  dht.setup(PA4);
 }
 
 void loop() {
 
-  // Average Temperature Readings
-  temperature = 0.0;
-  for (int i = 0; i < TEMP_READINGS; i++){
-    temperature += analogRead(PA4);
-    delay(2);
+  // DHT Readings
+  tmp_humidity = dht.getHumidity(); 
+  tmp_temperature = dht.getTemperature();
+  if (tmp_humidity > 0.0){
+    humidity = tmp_humidity;
   }
-
-  // Steinhart Convertion
-  ADCvalue = temperature / TEMP_READINGS;
-  Resistance = (1023 / ADCvalue) - 1;
-  Resistance = SERIESRESISTOR / Resistance;
-  steinhart = Resistance / NOMINAL_RESISTANCE; // (R/Ro)
-  steinhart = log(steinhart); // ln(R/Ro)
-  steinhart /= BCOEFFICIENT; // 1/B * ln(R/Ro)
-  steinhart += 1.0 / (NOMINAL_TEMPERATURE + 273.15); // + (1/To)
-  steinhart = 1.0 / steinhart; // Invert
-  steinhart -= 273.15; // convert to C
+  if (tmp_temperature > 0.0){
+    temperature = tmp_temperature;
+  }
 
   // Average Light Readings
   light = 0;
@@ -57,22 +51,59 @@ void loop() {
 
   // Communication Protocol
   if (Serial1.available() > 0){
+  
     Serial1.read();
 
-    steinhart_uint16 = uint16(steinhart * 100);
+    temperature_uint16 = uint16(temperature * 100);
+    humidity_uint8 = uint8(humidity);
 
-    steinhart_high = steinhart_uint16 >> 8;
-    steinhart_low = steinhart_uint16 & 0xFF;
+    temperature_high = temperature_uint16 >> 8;
+    temperature_low = temperature_uint16 & 0xFF;
+    light_high = light >> 8;
+    light_low = light & 0xFF;
 
-    Serial1.write((uint16_t) steinhart * 100);
-    Serial1.write(light);
-    Serial1.write(0xFF); //HUMIDITY
+    Serial1.write(temperature_high);    // 1
+    Serial1.write(temperature_low);     // 2
+    Serial1.write(light_high);          // 3
+    Serial1.write(light_low);           // 4
+    Serial1.write(humidity_uint8);      // 5 
     if (bt == 0){
-      Serial1.write(0xFF);
+      Serial1.write(0xFF);              // 6
     } else {
-      Serial1.write(bt);
+      Serial1.write(bt);                // 6
     }
+
   }
+
+  // DEBUG
+  // temperature_uint16 = uint16(temperature * 100);
+  // humidity_uint8 = uint8(humidity);
+
+  // temperature_high = temperature_uint16 >> 8;
+  // temperature_low = temperature_uint16 & 0xFF;
+  // light_high = light >> 8;
+  // light_low = light & 0xFF;
+
+  // Serial1.print("TEMP");
+  // Serial1.print("\t");
+  // Serial1.print(temperature_high);
+  // Serial1.print("\t");
+  // Serial1.println(temperature_low);
+  // Serial1.print("LUCE");
+  // Serial1.print("\t");
+  // Serial1.print(light_high);
+  // Serial1.print("\t");
+  // Serial1.println(light_low);
+  // Serial1.print("HUM");
+  // Serial1.print("\t");
+  // Serial1.println(humidity_uint8); //HUMIDITY
+  // Serial1.print("BT");
+  // Serial1.print("\t");
+  // if (bt == 0){
+  // Serial1.println(0xFF);
+  // } else {
+  // Serial1.println(bt);
+  // }
 
   // Sleep
   delay(50);
